b09a30677ff04e578d9a1e17e3de987ebb965e04
khalillf <khalillaafou7@gmail.com>
2025-03-12 13:44:56 +0000

    first commit

---
+++ transparisation_module/src/main/java/com/gov/cmr/transparisation_module/TransparisationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.gov.cmr.transparisation_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparisationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransparisationModuleApplication.class, args);
+    }
+
+}
---
+++ transparisation_module/src/test/java/com/gov/cmr/transparisation_module/TransparisationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.cmr.transparisation_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparisationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
